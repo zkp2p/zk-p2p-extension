d248d7acc459dd0cd2c3b8b886ceb7e910c50dbf
Alex Soong <alexsoong91@gmail.com>
2024-04-08 17:32:16 -0700

    Basic home page redesign

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,21 +3,34 @@ import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import browser from 'webextension-polyfill';
+import { User, PlusCircle, DollarSign } from 'react-feather';
+import styled from 'styled-components';
+import classNames from 'classnames';
 
 import Icon from '../../components/Icon';
-import classNames from 'classnames';
 import { notarizeRequest, setActiveTab, useActiveTabUrl, useRequests } from '../../reducers/requests';
 import bookmarks from '../../../utils/bookmark/bookmarks.json';
 import { replayRequest, urlify } from '../../utils/misc';
 import { get, NOTARY_API_LS_KEY, PROXY_API_LS_KEY } from '../../utils/storage';
+import { colors } from '@theme/colors';
+
 
 export default function Home(): ReactElement {
-  const requests = useRequests();
-  const url = useActiveTabUrl();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const requests = useRequests();
+  const url = useActiveTabUrl();
+
+  /*
+   * State
+   */
+
   const [originalTabId, setOriginalTabId] = useState<number | null>(null);
+
+  /*
+   * Handlers
+   */
 
   const handleCreateTab = async(bm: any) => {
     const [tab] = await browser.tabs.query({
@@ -36,11 +49,38 @@ export default function Home(): ReactElement {
     })
   }
 
+  /*
+   * Helpers
+   */
+
+  const iconForIndex = (index: number) => {
+    switch (index) {
+      case 0:
+        return <StyledUser />;
+
+      case 1:
+        return <StyledPlusCircle />;
+
+      case 2:
+      default:
+        return <StyledDollarSign />;
+    }
+  };
+
+  /*
+   * Component
+   */
 
   return (
-    <div className="flex flex-col gap-4 py-4 overflow-y-auto">
-      <div className="flex flex-col flex-nowrap justify-center gap-2 mx-4">
-        <NavButton fa="fa-solid fa-table" onClick={() => navigate('/requests')}>
+    <PageWrapper>
+      <IntroductionContainer>
+        <IntroductionTitle>Welcome to ZKP2P</IntroductionTitle>
+        <IntroductionDescription>
+          [Placeholder] This extension is meant to be used with the client: zkp2p.xyz
+        </IntroductionDescription>
+      </IntroductionContainer>
+
+        {/* <NavButton fa="fa-solid fa-table" onClick={() => navigate('/requests')}>
           <span>Requests</span>
           <span>{`(${requests.length})`}</span>
         </NavButton>
@@ -55,19 +95,9 @@ export default function Home(): ReactElement {
         </NavButton>
         <NavButton fa="fa-solid fa-gear" onClick={() => navigate('/options')}>
           Options
-        </NavButton>
-      </div>
-      {!bookmarks.length && (
-        <div className="flex flex-col flex-nowrap">
-          <div className="flex flex-col items-center justify-center text-slate-300 cursor-default select-none">
-            <div>No available notarization for {url?.hostname}</div>
-            <div>
-              Browse <Link to="/requests">Requests</Link>
-            </div>
-          </div>
-        </div>
-      )}
-      <div className="flex flex-col px-4 gap-4">
+        </NavButton> */}
+
+      <BookmarkContainer>
         {bookmarks.map((bm, i) => {
           try {
             const regex = new RegExp(bm.url); // Assuming bm.url contains a regex pattern
@@ -79,19 +109,19 @@ export default function Home(): ReactElement {
             const isReady = !!reqs.length;
 
             return (
-              <div
+              <BookmarkCard
                 key={i}
-                className="flex flex-col flex-nowrap border rounded-md p-2 gap-1 hover:bg-slate-50 cursor-pointer"
+                onClick={() => {
+                  navigate(`/registration`);
+                }}
               >
-                <div className="flex flex-row items-center text-xs">
-                  <div className="bg-slate-200 text-slate-400 px-1 py-0.5 rounded-sm">
-                    {bm.method}
-                  </div>
-                  <div className="text-slate-400 px-2 py-1 rounded-md">{bm.type}</div>
-                </div>
-                <div className="font-bold">{bm.title}</div>
-                <div className="italic">{bm.description}</div>
-                {isReady && (
+                <IconContainer>
+                  {iconForIndex(i)}
+                  <BookmarkTitle>{bm.title}</BookmarkTitle>
+                  <BookmarkDescription>{bm.description}</BookmarkDescription>
+                </IconContainer>
+
+                {/* {isReady && (
                   <button
                     className="button button--primary w-fit self-end mt-2"
                     onClick={async () => {
@@ -197,49 +227,138 @@ export default function Home(): ReactElement {
                   >
                     Notarize
                   </button>
-                )}
-                {!isReady && (
+                )} */}
+
+                {/* {!isReady && (
                   <button
                     className="button w-fit self-end mt-2"
                     onClick={() => handleCreateTab(bm)}
                   >
                     {`Go to ${bmHost}`}
                   </button>
-                )}
-              </div>
+                )} */}
+              </BookmarkCard>
             );
           } catch (e) {
             return null;
           }
         })}
-      </div>
-    </div>
+      </BookmarkContainer>
+    </PageWrapper>
   );
 }
 
-function NavButton(props: {
-  fa: string;
-  children?: ReactNode;
-  onClick?: MouseEventHandler;
-  className?: string;
-  disabled?: boolean;
-}): ReactElement {
-  return (
-    <button
-      className={classNames(
-        'flex flex-row flex-nowrap items-center justify-center',
-        'text-white rounded px-2 py-1 gap-1',
-        {
-          'bg-primary/[.8] hover:bg-primary/[.7] active:bg-primary': !props.disabled,
-          'bg-primary/[.5]': props.disabled,
-        },
-        props.className,
-      )}
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-      <Icon className="flex-grow-0 flex-shrink-0" fa={props.fa} size={1} />
-      <span className="flex-shrink w-0 flex-grow font-bold">{props.children}</span>
-    </button>
-  );
-}
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+`;
+
+const IntroductionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0rem 2rem 1rem 2rem;
+  gap: 0.5rem;
+  text-align: center;
+  color: ${colors.white};
+`;
+
+const IntroductionTitle = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const IntroductionDescription = styled.div`
+  font-size: 14px;
+`;
+
+const BookmarkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0rem 2rem;
+  gap: 1.5rem;
+`;
+
+const BookmarkCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  border: 1px solid ${colors.defaultBorderColor};
+  border-radius: 12px;
+  padding: 1.5rem;
+  gap: 1rem;
+  background-color: ${colors.selectorColor};
+  color: ${colors.white};
+  font-size: 14px;
+  &:hover {
+    background-color: ${colors.selectorHover};
+    cursor: pointer;
+  }
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+`;
+
+const StyledUser = styled(User)`
+  color: #fff;
+  width: 28px;
+  height: 28px;
+`;
+
+const StyledPlusCircle = styled(PlusCircle)`
+  color: #fff;
+  width: 28px;
+  height: 28px;
+`;
+
+const StyledDollarSign = styled(DollarSign)`
+  color: #fff;
+  width: 28px;
+  height: 28px;
+`;
+
+const BookmarkTitle = styled.div`
+  padding-top: 0.5rem;
+  font-size: 16px;
+  font-weight: 600;
+  text-align: center;
+`;
+
+const BookmarkDescription = styled.div`
+  font-size: 14px;
+  text-align: center;
+`;
+
+// function NavButton(props: {
+//   fa: string;
+//   children?: ReactNode;
+//   onClick?: MouseEventHandler;
+//   className?: string;
+//   disabled?: boolean;
+// }): ReactElement {
+//   return (
+//     <button
+//       className={classNames(
+//         'flex flex-row flex-nowrap items-center justify-center',
+//         'text-white rounded px-2 py-1 gap-1',
+//         {
+//           'bg-primary/[.8] hover:bg-primary/[.7] active:bg-primary': !props.disabled,
+//           'bg-primary/[.5]': props.disabled,
+//         },
+//         props.className,
+//       )}
+//       onClick={props.onClick}
+//       disabled={props.disabled}
+//     >
+//       <Icon className="flex-grow-0 flex-shrink-0" fa={props.fa} size={1} />
+//       <span className="flex-shrink w-0 flex-grow font-bold">{props.children}</span>
+//     </button>
+//   );
+// }
