1b14215f7415f34f08a520f26334af549807c68d
Alex Soong <alexsoong91@gmail.com>
2024-04-09 20:56:31 -0700

    Update TopNav to display the page the user is currently on

--- src/newcomponents/TopNav/TopNav.tsx
+++ src/newcomponents/TopNav/TopNav.tsx
@@ -8,48 +8,39 @@ import logo from '../../assets/img/logo192.png';
 
 export const TopNav: React.FC<{ withoutLinks?: boolean }> = ({ withoutLinks }) => {
   const location = useLocation();
-  const [selectedItem, setSelectedItem] = useState<string>('Landing');
+
+  const [selectedItem, setSelectedItem] = useState<string>('Home');
 
   useEffect(() => {
+    const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+    
     const routeName = location.pathname.split('/')[1];
-    setSelectedItem(routeName || 'Landing');
+
+    setSelectedItem(routeName ? capitalize(routeName) : 'Home');
   }, [location]);
 
   return (
     <NavBar>
-      {withoutLinks ? (
-        <NavBarCentered>
-          <Logo size={48} to="/" onClick={() => setSelectedItem('Landing')}>
-            <img src={logo} alt="logo" />
-          </Logo>
-        </NavBarCentered>
-      ) : (
-        <>
-          <LogoAndNavItems>
-            <Logo to="/" onClick={() => setSelectedItem('Landing')}>
-              <img src={logo} alt="logo" />
-            </Logo>
-          </LogoAndNavItems>
+      <LogoAndNavItems>
+        <Logo to="/" onClick={() => setSelectedItem('Home')}>
+          <img src={logo} alt="logo" />
+        </Logo>
+      </LogoAndNavItems>
 
-          <StyledSettings />
-        </>
-      )}
+      <NavigationTitle>
+        {selectedItem}
+      </NavigationTitle>
+
+      <StyledSettings />
     </NavBar>
   );
 };
-
-const NavBarCentered = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-`;
 
 const NavBar = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 30px;
+  padding: 1.5rem;
 `;
 
 const Logo = styled(Link)<{ size?: number }>`
@@ -69,6 +60,13 @@ const LogoAndNavItems = styled.div`
   display: flex;
   align-items: center;
   gap: 2rem;
+`;
+
+const NavigationTitle = styled.div`
+  display: flex;
+  font-size: 20px;
+  font-weight: 600;
+  color: #ffffff;
 `;
 
 const StyledSettings = styled(Settings)`
