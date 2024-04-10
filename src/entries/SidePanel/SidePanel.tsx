3488299a82f86fc5f0b9e5560347e1ce94312880
Alex Soong <alexsoong91@gmail.com>
2024-04-09 21:12:14 -0700

    Add individual pages for Deposits and Onramps

--- src/entries/SidePanel/SidePanel.tsx
+++ src/entries/SidePanel/SidePanel.tsx
@@ -15,7 +15,11 @@ import Notarize from '../../pages/Notarize';
 import ProofViewer from '../../pages/ProofViewer';
 import History from '../../pages/History';
 import ProofUploader from '../../pages/ProofUploader';
+
 import Registration from '../../pages/Notarizations/Registration';
+import DepositorRegistration from '../../pages/Notarizations/DepositorRegistration';
+import Onramp from '../../pages/Notarizations/Onramp';
+
 
 import logo from '../../assets/img/icon-48.png';
 import { TopNav } from '@newcomponents/TopNav/TopNav';
@@ -78,7 +82,9 @@ const SidePanel = () => {
         <Route path="/custom/*" element={<RequestBuilder />} />
         <Route path="/options" element={<Options />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/registration" element={<Registration requests={[]} />} />
+        <Route path="/registration" element={<Registration/>} />
+        <Route path="/deposit" element={<DepositorRegistration/>} />
+        <Route path="/onramp" element={<Onramp/>} />
         <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
     </AppContainer>
