f4903ea3129ee2f990e51c0641f9413b75e666b3
Alex Soong <alexsoong91@gmail.com>
2024-04-12 14:18:34 -0700

    Shrink rows height

--- src/newcomponents/Requests/Row.tsx
+++ src/newcomponents/Requests/Row.tsx
@@ -43,7 +43,7 @@ const Container = styled.div<{ selected: boolean; isLastRow: boolean }>`
   display: grid;
   grid-template-columns: 0.1fr 1fr 0.4fr;
   grid-gap: 1px;
-  padding: 1rem 1rem 0.75rem 1rem;
+  padding: 0.75rem 1rem;
   font-size: 14px;
   color: #ffffff;
   border-radius: ${({ isLastRow }) => (isLastRow ? '0 0 8px 8px' : '0')};
