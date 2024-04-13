18cacf149a28b8ca33f37fbc6af47fee787ab22e
Alex Soong <alexsoong91@gmail.com>
2024-04-13 16:19:03 -0700

    Update home page layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,6 +7,7 @@ import { useActiveTabUrl, useRequests } from '../../reducers/requests';
 import bookmarks from '../../../utils/bookmark/bookmarks.json';
 
 import { colors } from '@theme/colors';
+import { ThemedText } from '@theme/text';
 
 
 export default function Home(): ReactElement {
@@ -60,7 +61,7 @@ export default function Home(): ReactElement {
       <IntroductionContainer>
         <IntroductionTitle>Welcome to ZKP2P</IntroductionTitle>
         <IntroductionDescription>
-          [Placeholder] This extension is meant to be used with the client: zkp2p.xyz
+          This extension is meant to be used with the client: zkp2p.xyz
         </IntroductionDescription>
       </IntroductionContainer>
 
@@ -82,23 +83,45 @@ export default function Home(): ReactElement {
         </NavButton> */}
 
       <BookmarkContainer>
-        {bookmarks.map((bm, i) => {
-          return (
-            <BookmarkCard
-              key={i}
-              onClick={() => {
-                handleBookmarkPressedForIndex(i)
-              }}
-            >
-              <IconContainer>
+        <BookmarkTitle>
+          <ThemedText.ModalHeadline textAlign="left">
+            Wise
+          </ThemedText.ModalHeadline>
+        </BookmarkTitle>
+
+        <Bookmarks>
+          {bookmarks.map((bm, i) => {
+            return (
+              <ActionCard
+                key={i}
+                onClick={() => {
+                  handleBookmarkPressedForIndex(i)
+                }}
+              >
                 {iconForIndex(i)}
-                <BookmarkTitle>{bm.title}</BookmarkTitle>
-                <BookmarkDescription>{bm.description}</BookmarkDescription>
-              </IconContainer>
-            </BookmarkCard>
-          );
-        })}
+
+                <ActionTitle>
+                  {bm.title}
+                </ActionTitle>
+              </ActionCard>
+            );
+          })}
+        </Bookmarks>
       </BookmarkContainer>
+
+      <ComingSoonContainer>
+        <BookmarkTitle>
+          <ThemedText.ModalHeadline textAlign="left">
+            Coming Soon
+          </ThemedText.ModalHeadline>
+        </BookmarkTitle>
+
+        <ComingSoonDescription>
+          <ThemedText.SubHeaderSmall textAlign="left">
+              Some copy about some really cool things coming soon to look forward to in future updates
+          </ThemedText.SubHeaderSmall>
+        </ComingSoonDescription>
+      </ComingSoonContainer>
     </PageWrapper>
   );
 }
@@ -106,15 +129,13 @@ export default function Home(): ReactElement {
 const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
   gap: 1rem;
 `;
 
 const IntroductionContainer = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 0rem 2rem 1rem 2rem;
+  padding: 0rem 2rem;
   gap: 0.5rem;
   text-align: center;
   color: ${colors.white};
@@ -132,88 +153,77 @@ const IntroductionDescription = styled.div`
 const BookmarkContainer = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 0rem 2rem;
-  gap: 1.5rem;
+  padding: 1rem;
+  margin: 0rem 1rem;
+  gap: 0.75rem;
 `;
 
-const BookmarkCard = styled.div`
+// border-radius: 12px;
+// border: 1px solid ${colors.defaultBorderColor};
+
+const BookmarkTitle = styled.div`
+  padding-left: 0.75rem;
+  color: ${colors.white};
+`;
+
+const Bookmarks = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+  gap: 0.5rem;
+`;
+
+const ActionCard = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+
   flex-wrap: nowrap;
-  border: 1px solid ${colors.defaultBorderColor};
   border-radius: 12px;
-  padding: 1.5rem;
-  gap: 1rem;
+  padding: 1rem 1.25rem;
+  gap: 0.75rem;
   background-color: ${colors.selectorColor};
   color: ${colors.white};
   font-size: 14px;
+
   &:hover {
     background-color: ${colors.selectorHover};
     cursor: pointer;
   }
 `;
 
-const IconContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 0.25rem;
-`;
-
 const StyledUser = styled(User)`
-  color: #fff;
-  width: 28px;
-  height: 28px;
+  color: ${colors.white};
+  width: 20px;
+  height: 20px;
 `;
 
 const StyledPlusCircle = styled(PlusCircle)`
-  color: #fff;
-  width: 28px;
-  height: 28px;
+  color: ${colors.white};
+  width: 20px;
+  height: 20px;
 `;
 
 const StyledDollarSign = styled(DollarSign)`
-  color: #fff;
-  width: 28px;
-  height: 28px;
+  color: ${colors.white};
+  width: 20px;
+  height: 20px;
 `;
 
-const BookmarkTitle = styled.div`
-  padding-top: 0.5rem;
-  font-size: 16px;
+const ActionTitle = styled.div`
+  font-size: 15px;
   font-weight: 600;
-  text-align: center;
 `;
 
-const BookmarkDescription = styled.div`
-  font-size: 14px;
-  text-align: center;
+const ComingSoonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  margin: 0rem 1rem;
+  gap: 0.75rem;
 `;
 
-// function NavButton(props: {
-//   fa: string;
-//   children?: ReactNode;
-//   onClick?: MouseEventHandler;
-//   className?: string;
-//   disabled?: boolean;
-// }): ReactElement {
-//   return (
-//     <button
-//       className={classNames(
-//         'flex flex-row flex-nowrap items-center justify-center',
-//         'text-white rounded px-2 py-1 gap-1',
-//         {
-//           'bg-primary/[.8] hover:bg-primary/[.7] active:bg-primary': !props.disabled,
-//           'bg-primary/[.5]': props.disabled,
-//         },
-//         props.className,
-//       )}
-//       onClick={props.onClick}
-//       disabled={props.disabled}
-//     >
-//       <Icon className="flex-grow-0 flex-shrink-0" fa={props.fa} size={1} />
-//       <span className="flex-shrink w-0 flex-grow font-bold">{props.children}</span>
-//     </button>
-//   );
-// }
+const ComingSoonDescription = styled.div`
+  padding-left: 0.75rem;
+  color: ${colors.white};
+`;
