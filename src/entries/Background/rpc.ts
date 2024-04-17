5b42dea3207b8058e8ebe99a61a0a294176bef1e
0xSachinK <0xsachink@gmail.com>
2024-04-17 23:44:52 +0200

    Fix bug in passing maxSentData and maxRecvData

--- src/entries/Background/rpc.ts
+++ src/entries/Background/rpc.ts
@@ -279,6 +279,8 @@ async function handleProveRequestStart(
       method,
       headers,
       body,
+      maxSentData,
+      maxRecvData,
       notaryUrl,
       websocketProxyUrl,
       secretHeaders,
