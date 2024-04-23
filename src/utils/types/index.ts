bdcb23a3cc3015bc234f8935f708f425ab429da3
Richard Liang <richardliang2015@u.northwestern.edu>
2024-04-23 16:30:46 -0400

    rebase

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,4 +1,6 @@
 export { WiseAction, wiseActions, WiseStep, wiseSteps, WiseRequest, WISE_PLATFORM } from './wiseActions';
 export type { OnRamperIntent, Intent, WiseActionType, WiseStepType, WiseRequestType } from './wiseActions';
+export { RevolutAction, revolutActions, RevolutStep, revolutSteps, RevolutRequest, REVOLUT_PLATFORM } from './revolutActions';
+export type { RevolutActionType, RevolutStepType, RevolutRequestType } from './revolutActions';
 export { API_CONFIGURATIONS, StatusColors } from './apiConfigurations';
 export type { StatusColorsType } from './apiConfigurations';
