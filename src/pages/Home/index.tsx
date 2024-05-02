6d801c9ba389031708dae3712e8acd38199fdb34
Alex Soong <alexsoong91@gmail.com>
2024-05-02 14:51:34 +0900

    Update home page Coming Soon component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement } from 'react';
 import { useNavigate } from 'react-router';
-import { UserPlus, Tool, DollarSign } from 'react-feather';
+import { UserPlus, DollarSign } from 'react-feather';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStore } from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
 
 import { Button } from '@newcomponents/common/Button';
@@ -20,6 +22,10 @@ export default function Home(): ReactElement {
   /*
    * Handlers
    */
+
+  const handleReturnToTab = async () => {
+    chrome.tabs.create({ url: 'https://zkp2p.xyz' });
+  };
 
   const handleBookmarkPressedForIndex = (index: number) => {
     switch (index) {
@@ -54,105 +60,114 @@ export default function Home(): ReactElement {
   return (
     <PageWrapper>
       <IntroductionContainer>
-        <IntroductionTitle>
-          Welcome to ZKP2P
-        </IntroductionTitle>
-        
-        <IntroductionDescription>
-          This extension is meant to be used with the client: zkp2p.xyz
-        </IntroductionDescription>
-      </IntroductionContainer>
+        <IntroductionTextContainer>
+          <IntroductionTitle>
+            Welcome to ZKP2P
+          </IntroductionTitle>
+          
+          <IntroductionDescription>
+            This browser assistant is meant to be used on zkp2p.xyz with Revolut
+          </IntroductionDescription>
+        </IntroductionTextContainer>
 
-        {/* <NavButton fa="fa-solid fa-table" onClick={() => navigate('/requests')}>
-          <span>Requests</span>
-          <span>{`(${requests.length})`}</span>
-        </NavButton>
-        <NavButton fa="fa-solid fa-magnifying-glass" onClick={() => navigate('/custom')}>
-          Custom
-        </NavButton>
-        <NavButton fa="fa-solid fa-magnifying-glass" onClick={() => navigate('/verify')}>
-          Verify
-        </NavButton>
-        <NavButton fa="fa-solid fa-list" onClick={() => navigate('/history')}>
-          History
-        </NavButton>
-        <NavButton fa="fa-solid fa-gear" onClick={() => navigate('/options')}>
-          Options
-        </NavButton> */}
-
-      <RevolutContainer>
-        <RevolutTitle>
-          <ThemedText.ModalHeadline textAlign="left">
-            Revolut
-          </ThemedText.ModalHeadline>
-        </RevolutTitle>
-
-        <ActionsGrid>
-          {revolut.map((bm, i) => {
-            return (
-              <ActionCard
-                key={i}
-                onClick={() => {
-                  handleBookmarkPressedForIndex(i)
-                }}
-              >
-                {iconForIndex(i)}
-
-                <ActionTitle>
-                  {bm.title}
-                </ActionTitle>
-              </ActionCard>
-            );
-          })}
-        </ActionsGrid>
-      </RevolutContainer>
-
-      <ComingSoonContainer>
-        <RevolutTitle>
-          <ThemedText.ModalHeadline textAlign="left">
-            Marketplace
-          </ThemedText.ModalHeadline>
-        </RevolutTitle>
-        <ComingSoonModalContainer>
-          <ComingSoonTitle>
-            <ThemedText.ModalHeadline style={{ flex: '1', margin: 'auto', textAlign: 'center', fontSize: '15px' }}>
-              Coming Soon
-            </ThemedText.ModalHeadline>
-            <StyledTool />
-          </ComingSoonTitle>
-
-
-          <ComingSoonDescription>
-            Please let us know the platforms you would like to see supported!
-          </ComingSoonDescription>
-
+        <ButtonContainer>
           <Button
-            onClick={() => window.open('https://forms.gle/UG699TVHmbdN9jN36', '_blank')}
+            onClick={() => handleReturnToTab()}
             width={164}
             height={40}
             fontSize={14}
           >
-            Give feedback ↗
+            Go to zkp2p.xyz
           </Button>
-        </ComingSoonModalContainer>
+        </ButtonContainer>
+      </IntroductionContainer>
+
+      <IntegrationsContainer>
+        <TitleContainer>
+          <IntroductionTitle>
+            Integrations
+          </IntroductionTitle>
+        </TitleContainer>
+
+        <RevolutContainer>
+          <ThemedText.TableHeaderSmall textAlign="left">
+              Revolut
+          </ThemedText.TableHeaderSmall>
+
+          <ActionsGrid>
+            {revolut.map((bm, i) => {
+              return (
+                <ActionCard
+                  key={i}
+                  onClick={() => {
+                    handleBookmarkPressedForIndex(i)
+                  }}
+                >
+                  {iconForIndex(i)}
+
+                  <ActionTitle>
+                    {bm.title}
+                  </ActionTitle>
+                </ActionCard>
+              );
+            })}
+          </ActionsGrid>
+        </RevolutContainer>
+      </IntegrationsContainer>
+
+      <ComingSoonContainer>
+        <TitleContainer>
+          <ThemedText.ModalHeadline textAlign="left">
+            Coming Soon
+          </ThemedText.ModalHeadline>
+        </TitleContainer>
+
+        <ComingSoongBodyContainer
+          onClick={() => {
+            chrome.tabs.create({ url: 'https://t.me/+XDj9FNnW-xs5ODNl' });
+          }}
+          >
+          <FontAwesomeIcon
+            icon={faStore}
+            style={{ color: colors.white, width: '28px', height: '28px'}}
+          />
+
+          <ThemedText.TableDescriptionSmall textAlign="center" lineHeight={1.3}>
+            Have ideas for other assets you would like to see supported by ZKP2P?<br/>Let us know!
+          </ThemedText.TableDescriptionSmall>
+        </ComingSoongBodyContainer>
       </ComingSoonContainer>
     </PageWrapper>
   );
 }
 
 const PageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
+  display: grid;
+  grid-template-rows: .8fr 1fr 1fr;
+  min-height: 100vh;
+  grid-template-areas:
+    "intro"
+    "integrations"
+    "comingSoon";
+
+  padding-bottom: 4rem;
 `;
 
 const IntroductionContainer = styled.div`
+  grid-area: intro;
   display: flex;
   flex-direction: column;
-  padding: 0rem 2rem;
-  gap: 0.5rem;
+  padding: 1rem 2rem;
+  gap: 0.75rem;
   text-align: center;
   color: ${colors.white};
+`;
+
+const IntroductionTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 0.25rem;
 `;
 
 const IntroductionTitle = styled.div`
@@ -166,7 +181,8 @@ const IntroductionDescription = styled.div`
   font-size: 15px;
 `;
 
-const RevolutContainer = styled.div`
+const IntegrationsContainer = styled.div`
+  grid-area: integrations;
   display: flex;
   flex-direction: column;
   padding: 1rem;
@@ -174,12 +190,17 @@ const RevolutContainer = styled.div`
   gap: 0.75rem;
 `;
 
-// border-radius: 12px;
-// border: 1px solid ${colors.defaultBorderColor};
-
-const RevolutTitle = styled.div`
+const TitleContainer = styled.div`
   padding-left: 0.75rem;
   color: ${colors.white};
+`;
+
+const RevolutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 0.75rem;
+  color: ${colors.white};
+  gap: 0.75rem;
 `;
 
 const ActionsGrid = styled.div`
@@ -218,13 +239,6 @@ const StyledDollarSign = styled(DollarSign)`
   height: 20px;
 `;
 
-const StyledTool = styled(Tool)`
-  color: ${colors.white};
-  width: 20px;
-  height: 20px;
-  margin-left: 10px;
-`;
-
 const ActionTitle = styled.div`
   font-size: 15px;
   font-weight: 600;
@@ -232,6 +246,7 @@ const ActionTitle = styled.div`
 `;
 
 const ComingSoonContainer = styled.div`
+  grid-area: comingSoon;
   display: flex;
   flex-direction: column;
   padding: 1rem;
@@ -239,30 +254,28 @@ const ComingSoonContainer = styled.div`
   gap: 0.75rem;
 `;
 
-const ComingSoonTitle = styled.div`
+const ComingSoongBodyContainer = styled.div`
   display: flex;
-  padding-left: 0.75rem;
-  align-items: center;
-  color: ${colors.white};
-`;
-
-const ComingSoonDescription = styled.div`
-  padding-left: 0.75rem;
-  color: ${colors.white};
-  font-size: 14px;
-`;
-
-const ComingSoonModalContainer = styled.div`
-  display: flex;
+  width: 100%;
   flex-direction: column;
-  padding: 1rem;
+  justify-content: center;
+  align-items: center;
+  padding: 1.25rem 2rem;
   margin: 0rem 0.5rem;
   gap: 0.75rem;
   border-radius: 16px;
-  border: 1px dashed rgba(255, 255, 255, 0.2);
-  background-color: ${colors.container};
-  align-items: center;
-  gap: 1.5rem;
+  background-color: ${colors.selectorColor};
+
+  color: #ffffff;
+
+  &:hover {
+    background-color: ${colors.selectorHover};
+    cursor: pointer;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
   justify-content: center;
-  top: 200px;
+  align-items: center;
 `;
